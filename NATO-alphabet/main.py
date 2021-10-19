19806126b0d269fbbf131daee95a14562103fdae
Ramaniuk <240724oi@gmail.com>
2021-10-19 18:05:04 -0400

    upgrade

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -7,11 +7,19 @@ for (index, row) in phonetic_alphabet.iterrows():
     words_dict[row.letter] = row.code
 #words_dict = {row.letter : row.code for(index, row) in phonetic_alphabet.iterrows()}
 
-name = (input("Enter your name: ")).upper()
-words_list = []
-for letter in name:
-    words_list.append(words_dict[letter])
-#words_list = [words_dict[letter] for letter in name]
 
-print(words_list)
+def generate_phonetic():
+    name = (input("Enter your name: ")).upper()
+    try:
+        # words_list = []
+        # for letter in name:
+        #     words_list.append(words_dict[letter])
+        words_list = [words_dict[letter] for letter in name]
+    except KeyError:
+        print("Only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(words_list)
+
+generate_phonetic()
 
