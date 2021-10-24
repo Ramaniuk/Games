e898eb5eabe7571ced7d79b3c5b78fb1c7fc3dd7
Ramaniuk <240724oi@gmail.com>
2021-10-24 17:59:58 -0400

    send birthday wishes

---
+++ birthday-wisher-extrahard-start/main.py
@@ -0,0 +1,29 @@
+##################### Extra Hard Starting Project ######################
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+now = dt.datetime.now()
+date_today = now.date()
+
+file = pandas.read_csv("birthdays.csv")
+for ind in file.index:
+    birthday = f'{now.year}-{file["month"][ind]}-{file["day"][ind]}'
+    if birthday == str(date_today):
+        letter = f'letter_{random.randint(1,3)}.txt'
+        with open(f"letter_templates/{letter}") as file_wishes:
+            sample = file_wishes.readlines()
+        sample[0] = f'Dear {file["name"][ind]},\n'
+        sample[-1] = f'Olga'
+        message = ""
+        message = message.join(sample)
+
+        my_email = 'testForCoursesOlga@gmail.com'
+        password = 'testForCourses12'
+        connection = smtplib.SMTP("smtp.gmail.com", 587)
+        connection.starttls() # call connection
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=str(file["email"][ind]),
+                            msg=f'Subject:Happy birthday!!!\n\n {message}')
+        connection.close()
---
+++ send-email/main.py
@@ -0,0 +1,36 @@
+import datetime as dt
+import random
+import smtplib
+
+now = dt.datetime.now()
+year = now.year
+day_of_week = now.weekday()
+date_of_birth = dt.datetime(year=1995,month=12,day=13,hour=4)
+
+if day_of_week == 6:
+    with open("quotes.txt") as file:
+        f = file.readlines()
+        quote = random.choice(f)
+
+my_email = 'testForCoursesOlga@gmail.com'
+password = 'testForCourses12'
+connection = smtplib.SMTP("smtp.gmail.com",587)
+connection.starttls()
+connection.login(user=my_email,password=password)
+connection.sendmail(from_addr=my_email, to_addrs="testForCourse@yahoo.com",
+                    msg=f"Subject:Hello\n\n {quote}")
+connection.close()
+
+
+# with smtplib.SMTP("smtp.gmail.com",587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email,password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs="testForCourse@yahoo.com",
+#                         msg="Subject:Hello\n\n Hello")
+
+
+now = dt.datetime.now()
+year = now.year
+day_of_week = now.weekday()
+
+date_of_birth = dt.datetime(year=1995,month=12,day=13,hour=4)
